81e6b2065123a6c86f25ec9806d7072d8927d710
amutsamoses <amutsamoses@gmail.com>
2023-03-30 03:56:41 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,18 +6,14 @@
  * @dest: pointer to the buffer
  * @src: pointer to the source string
  * @n: length of src
- * Return: char
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-for (i = 0; i < n; i++)
-dest[i] = '\0';
+for (i = 0 ; i < n ; i++)
+	dest[i] = src[i];
 return (dest);
 }
