cca4af4170afe081898cd542dcfb6c6a7b25cc63
amutsamoses <amutsamoses@gmail.com>
2023-03-30 02:42:07 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two string
+ *
+ * @dest: resulting string
+ * @src: pointer to the source string
+ * @n: string length
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, m;
+
+m = string_length(dest);
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[m + i] = src[i];
+}
+dest[m + i] = '\0';
+return (dest);
+}
+
+/**
+ * string_length - find the length of a string
+ * Return: length of c
+ * @pointer: pointer
+ */
+
+int string_length(char *pointer)
+{
+int c = 0;
+
+while (*(pointer + c) != '\0')
+{
+c++;
+}
+return (c);
+}
